5ac0f09374aac27901ca717263f0477fce998c8e
Clément V <clem.vercollier@gmail.com>
2020-12-07 16:22:10 +0100

    Update day1.py

--- day1/day1.py
+++ day1/day1.py
@@ -5,11 +5,25 @@ input = [int(i) for i in input]
 input = sorted(input, reverse=True)
 
 def day1(input):
-    for i in input:
+    for i in range(len(input)):
         j = len(input) - 1
-        while i + input[j] < 2020:
+        while input[i] + input[j] < 2020 and j > i:
             j -= 1
-        if i + input[j] == 2020 :
-            return i * input[j]
+        if input[i] + input[j] == 2020 :
+            return input[i] * input[j]
 
 print(day1(input))
+
+def day1_part2(input):
+    for i in range(len(input)):
+        j = len(input) - 1
+        while input[i] + input[j] < 2020 and j > i:
+            if j-i > 1:
+                for k in range(j-1, i, -1):
+                    if input[i] + input[j] + input[k] > 2020:
+                        break
+                    elif input[i] + input[j] + input[k] == 2020:
+                        return input[i] * input[j] * input[k]
+            j -= 1
+
+print(day1_part2(input))
