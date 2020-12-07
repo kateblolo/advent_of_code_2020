d63ce301c12cb7fe5f0a62bb615b5fc1470feca8
Clément V <clem.vercollier@gmail.com>
2020-12-07 17:44:34 +0100

    day3 part2 + quick fix on part1

--- day3/day3.py
+++ day3/day3.py
@@ -1,14 +1,23 @@
 with open ("input_day3.txt", "r") as myfile:
     input=myfile.read().split("\n")
 
-def count_trees(input, right):
+def count_trees(input, right, down):
+    input = input[down::down]
     len_line = len(input[0])
     trees = 0
     current_position = 0
-    for i in input[1:]:
+    for i in input:
         current_position = (current_position + right) % len_line
         if i[current_position] == "#":
             trees += 1
     return trees
 
-print(count_trees(input, 3))
+def multiply_slopes(input, lst_moves):
+    trees = 1
+    for i in lst_moves:
+        trees *= count_trees(input, i[0], i[1])
+    return trees
+
+#print(count_trees(input, 3))
+lst_moves = [(1, 1), (3, 1), (5,1), (7, 1), (1, 2)]
+print(multiply_slopes(input, lst_moves))
