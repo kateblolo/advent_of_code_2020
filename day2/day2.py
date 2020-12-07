0db0e07628a712e006bf979c430c5ef12c43ed24
Clément V <clem.vercollier@gmail.com>
2020-12-07 16:52:28 +0100

    part2 solved

--- day2/day2.py
+++ day2/day2.py
@@ -13,7 +13,23 @@ def is_valid_pwd(line):
         return True
     return False
 
+def is_valid_pwd2(line):
+    line = line.split(":")
+    rules, pwd = line[0], line[1].strip()
+    rules = rules.split(" ")
+    occurences, letter = rules[0], rules[1]
+    occurences = occurences.split("-")
+    pos1, pos2 = int(occurences[0])-1, int(occurences[1])-1
+
+    if (pwd[pos1] == letter or pwd[pos2] == letter) and (pwd[pos1] != pwd[pos2]):
+        return True
+    return False
+
 def day2(input):
     return sum([is_valid_pwd(i) for i in input])
 
-print(day2(input))
+def day2_part2(input):
+    return sum([is_valid_pwd2(i) for i in input])
+
+#print(day2(input))
+print(day2_part2(input))
