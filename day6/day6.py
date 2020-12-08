f77fb4c4887d6468fd6c61d639379cff90b918ea
Clément V <clem.vercollier@gmail.com>
2020-12-08 01:34:29 +0100

    day6 part2 solved

--- day6/day6.py
+++ day6/day6.py
@@ -1,7 +1,8 @@
 with open ("input_day6.txt", "r") as myfile:
     input=myfile.read().replace("\n", " ").split("  ")
 
-input = [i.replace(" ", "") for i in input]
+#input = [i.replace(" ", "") for i in input] ##part1
+input = [i.split(" ") for i in input] ##part2
 
 def solve(input):
     res = 0
@@ -9,4 +10,17 @@ def solve(input):
         res += len(''.join(set(answers)))
     return res
 
-print(solve(input))
+def solve2(input):
+    res = 0
+    for answers in input:
+        if len(answers) == 1:
+            res += len(set(answers[0]))
+        else:
+            s = set(answers[0])
+            for i in range(len(answers)-1):
+                s = s.intersection(answers[i+1])
+            res += len(s)
+    return res
+
+#print(solve(input))
+print(solve2(input))
