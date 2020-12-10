807fe47ba397e15beb5581d3262f21fb6cc8b871
Clément V <clem.vercollier@gmail.com>
2020-12-11 00:04:23 +0100

    day10 part2
    tribonacci based solution

--- day10/day10.py
+++ day10/day10.py
@@ -3,7 +3,6 @@ with open("input_day10.txt", "r") as myfile:
 
 input = [int(x) for x in input]
 input = [0] + sorted(input)
-print(input)
 
 def solve(input):
     d = {1:0, 2:0, 3:1}
@@ -11,4 +10,26 @@ def solve(input):
         d[input[i+1]-input[i]] += 1
     return d[1] * d[3]
 
-print(solve(input))
+def solve2(input):
+    res = 1
+    previous = [1]
+    for i in range(len(input)-2):
+        if len(previous)<3:
+            if input[i+2]-input[i] <= 3:
+                res *= 2
+                previous.append(res)
+            else:
+                previous = [previous[len(previous)-1]]
+        else:
+            if input[i + 2] - input[i] <= 3:
+                res, previous = weird_seq(previous)
+            else:
+                previous = [previous[len(previous)-1]]
+    return res
+
+def weird_seq(lst):
+    res = lst[0] + lst[1] + lst[2]
+    lst2 = [lst[1], lst[2], res]
+    return res, lst2
+
+print(solve2(sorted(input)))
